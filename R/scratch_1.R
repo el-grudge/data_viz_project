0ad7bfbec2614b6234b75940d97919f827021007
minasonbol@gmail.com <minasonbol@gmail.com>
2020-04-16 21:47:55 -0400

    test new repo

--- R/scratch_1.R
+++ R/scratch_1.R
@@ -716,4 +716,3 @@ as.data.frame(platonictable %>%
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
       axis.ticks.x=element_blank())
-################################################
